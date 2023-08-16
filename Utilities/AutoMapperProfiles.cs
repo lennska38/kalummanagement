4f8de6857617cd1f32ee4e1ce40caa3774e865b1
Kevin Ramos <lennykev38@gmail.com>
2023-08-16 17:41:41 -0600

    Todo el proyecto

---
+++ Utilities/AutoMapperProfiles.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using KalumManagement.Dtos;
+using KalumManagement.Dtos.InversionCarreraTecnica;
+using KalumManagement.Entities;
+
+namespace KalumManagement.Utilities
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Aspirante, CarreraTecnicaAspiranteListDTO>();
+            CreateMap<CarreraTecnica, CarreraTecnicaListDTO>();
+            CreateMap<Aspirante, JornadaAspiranteListDTO>();
+            CreateMap<Jornada, JornadaListDTO>();
+            CreateMap<CarreraTecnicaCreateDTO, CarreraTecnica>();
+            CreateMap<AspiranteCreateDTO, Aspirante>();
+            CreateMap<Aspirante, AspiranteDTO>().ConstructUsing(e => new AspiranteDTO{NombreCompleto = $"{e.Apellido} {e.Nombre}"});
+            
+            CreateMap<CarreraTecnica, CarreraTecnicaDTO>();
+            CreateMap<CarreraTecnicaDTO, CarreraTecnica>();
+
+            CreateMap<Jornada, JornadaDTO>();
+            CreateMap<JornadaDTO, Jornada>();
+            CreateMap<Jornada, JornadaCreateDTO>();
+            CreateMap<JornadaCreateDTO, Jornada>();
+
+            CreateMap<ExamenAdmision, ExamenAdmisionDTO>();
+            CreateMap<ExamenAdmisionDTO, ExamenAdmision>();
+            CreateMap<ExamenAdmision, ExamenAdmisionCreateDTO>();
+            CreateMap<ExamenAdmisionCreateDTO, ExamenAdmision>();
+
+            CreateMap<InversionCarreraTecnica, InversionCarreraTecnicaListDTO>();
+            CreateMap<InversionCarreraTecnicaListDTO, InversionCarreraTecnica>();
+
+            CreateMap<Alumno, AlumnoCreateDTO>();
+            CreateMap<AlumnoCreateDTO, Alumno>();      
+            CreateMap<Alumno, AlumnoDTO>();
+            CreateMap<AlumnoDTO, Alumno>();      
+        }
+    }
+}
